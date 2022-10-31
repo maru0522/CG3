e6b3a72113dbc3c282298d395db19e3ab968479e
maru0522 <94513876+maru0522@users.noreply.github.com>
2022-10-31 17:50:18 +0900

    2-4完了

--- Resources/shaders/BasicGeometryShader.hlsl
+++ Resources/shaders/BasicGeometryShader.hlsl
@@ -1,7 +1,27 @@
 #include "BasicShaderHeader.hlsli"
 
-// 三角形の入力から、点を3つ出力するサンプル
-[maxvertexcount(3)]
+// 四角形の頂点数
+static const uint vnum = 4;
+
+// 左上が0,0 右下が1,1
+static const float2 uv_array[vnum] =
+{
+	float2(0, 1),
+	float2(0, 0),
+	float2(1, 1),
+	float2(1, 0)
+};
+
+// センターからのオフセット
+static const float4 offset_array[vnum] =
+{
+	float4(-0.5f, -0.5f, 0, 0), //左下
+	float4(-0.5f, +0.5f, 0, 0), //左上
+	float4(+0.5f, -0.5f, 0, 0), //右下
+	float4(+0.5f, +0.5f, 0, 0) //右下
+};
+
+[maxvertexcount(vnum)]
 void main(
 	point VSOutput input[1] : SV_POSITION,
 	// 点ストリーム
@@ -9,22 +29,14 @@ void main(
 )
 {
 	GSOutput element; // 出力用頂点データ
-	// 共通
-	element.normal = input[0].normal; // 法線をコピー
-	element.uv = input[0].uv; //UVをコピー
-	
-	// 1点目
-	element.svpos = input[0].svpos; // 頂点座標をコピー
-	// 頂点を1つ出力（出力リストに追加)
-	output.Append(element);
-	
-	// 2点目
-	element.svpos = input[0].svpos + float4(10.0f,10.0f,0,0); // 頂点座標をコピー
-	// 頂点を1つ出力（出力リストに追加)
-	output.Append(element);
-	
-	// 3点目
-	element.svpos = input[0].svpos + float4(10.0f, 0, 0, 0); // 頂点座標をコピー
-	// 頂点を1つ出力（出力リストに追加)
-	output.Append(element);
+	// 4点分まわす
+	for (uint i = 0; i < vnum; i++)
+	{
+		// ワールド座標ベースで、ずらす
+		element.svpos = input[0].pos + offset_array[i]; // 頂点座標をコピー
+		// ビュー、射影変換行列
+		element.svpos = mul(mat, element.svpos);
+		element.uv = uv_array[i];
+		output.Append(element);
+	}
 }
